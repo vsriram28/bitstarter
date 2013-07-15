30749bf8b3dbc8bf21f6d637ead5c942ddb5f06c
Sriram <vsriram28@yahoo.com>
2013-07-15 01:39:35 +0000

    updated web.js  to use buffers.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var infile = "index.html"
+
+var outstr = new Buffer (fs.readFileSync(infile,"utf-8"));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outstr.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
